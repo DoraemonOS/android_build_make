optimus9811/android_build

core: config: Include Cherish sepolicy little earlier.
* Cherish sepolicy is now considered

Change-Id: If0cfebafa0c3b3dc52434fca7798a82076ed387a
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

core/config.mk
@@ -1159,6 +1159,14 @@ endif
 dont_bother_goals := out \
     product-graph dump-products
 
+ifneq ($(CHERISH_BUILD),)
+ifneq ($(wildcard device/cherish/sepolicy/common/sepolicy.mk),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/cherish/sepolicy/common/sepolicy.mk)
+endif
+endif
+
 # Make ANDROID Soong config variables visible to Android.mk files, for
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
@@ -1175,14 +1183,6 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 
 # Make RECORD_ALL_DEPS readonly.
 RECORD_ALL_DEPS :=$= $(filter true,$(RECORD_ALL_DEPS))
-
-ifneq ($(CHERISH_BUILD),)
-ifneq ($(wildcard device/cherish/sepolicy/common/sepolicy.mk),)
-## We need to be sure the global selinux policies are included
-## last, to avoid accidental resetting by device configs
-$(eval include device/cherish/sepolicy/common/sepolicy.mk)
-endif
-endif
 
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk

DoraemonOS/android_build_make

Signed-off-by: dopaemon <polarisdp@gmail.com>

core/config.mk
@@ -1159,6 +1159,14 @@ endif
 dont_bother_goals := out \
     product-graph dump-products
 
+ifneq ($(DORA_BUILD),)
+ifneq ($(wildcard device/dora/sepolicy/common/sepolicy.mk),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/dora/sepolicy/common/sepolicy.mk)
+endif
+endif
+
 # Make ANDROID Soong config variables visible to Android.mk files, for
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
@@ -1175,14 +1183,6 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 
 # Make RECORD_ALL_DEPS readonly.
 RECORD_ALL_DEPS :=$= $(filter true,$(RECORD_ALL_DEPS))
-
-ifneq ($(DORA_BUILD),)
-ifneq ($(wildcard device/dora/sepolicy/common/sepolicy.mk),)
-## We need to be sure the global selinux policies are included
-## last, to avoid accidental resetting by device configs
-$(eval include device/dora/sepolicy/common/sepolicy.mk)
-endif
-endif
 
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk